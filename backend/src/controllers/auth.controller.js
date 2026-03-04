774b08cd8388ed29c6c60b74f2618fb7f8e07105
Rio <satria.justicio@gmail.com>
2026-03-04 14:49:53 +0700

    Nambahin scripts untuk konfigurasi backend, nambahin logic authentication di backend (untested)

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+import { login } from "../services/auth.service.js";
+
+export const login = async (req, res, next) => {
+  try {
+    const result = await login(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+};
